7cc27e75ced46574a6b7577c2addabee5cd149d1
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-01-17 22:55:11 +0530

    hero

--- Shopora/navbar.js
+++ Shopora/navbar.js
@@ -1,3 +1,5 @@
+// toggle theme and logo
+
 const darktoggle = document.getElementById("dark-toggle");
 const mobileDarkToggle = document.getElementById("mobile-dark-toggle");
 const body = document.body;
@@ -34,3 +36,52 @@ function toggleDarkMode() {
 
 // darktoggle.addEventListener("click", toggleDarkMode);
 // mobileDarkToggle.addEventListener("click", toggleDarkMode);
+
+
+// menu
+
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeBtn = document.getElementById("close-btn");
+const menuOverlay = document.getElementById("menu-overlay"); // id match karo
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+  menuOverlay.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  menuOverlay.classList.remove("active");
+  document.body.style.overflow = "auto";
+});
+menuOverlay.addEventListener("click",()=>{
+    mobileMenu.classList.remove("active");
+    menuOverlay.classList.remove("active");
+    document.body.style.overflow = "auto"
+})
+
+// shop dropdown
+
+// const shopChevron = document.getElementById("shop-chevron");
+// const mobileMegaMenu = document.getElementById("mobile-mega-menu");
+
+
+// shopChevron.addEventListener("click", (e) => {
+//  e.preventDefault();
+//  mobileMegaMenu.classList.toggle("active");
+//  shopChevron.classList.toggle("fa-chevron-down");
+//  shopChevron.classList.toggle("fa-chevron-up");
+// });
+
+function toggleDropdown(e) {
+  e.preventDefault(); 
+  e.stopPropagation(); 
+  const mobileMegaMenu = document.getElementById("mobile-mega-menu");
+  const shopChevron = document.getElementById("shop-chevron");
+
+  mobileMegaMenu.classList.toggle("active");
+  shopChevron.classList.toggle("fa-chevron-down");
+  shopChevron.classList.toggle("fa-chevron-up");
+}
