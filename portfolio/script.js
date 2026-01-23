58ca5f5518c70057906baab378901d2034a30910
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-01-23 12:09:10 +0530

    portfolio

--- portfolio/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navlist = document.querySelector(".navlist");
-
-hamburger.addEventListener("click", () => {
-  navlist.classList.toggle("active");
-});
