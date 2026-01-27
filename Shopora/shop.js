2e679a39bf6cb1ef399d5f5e61c777dec5778cd0
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-01-27 14:33:43 +0530

    shop

--- Shopora/shop.js
+++ Shopora/shop.js
@@ -81,7 +81,7 @@ document.addEventListener("DomContentLoaded",()=>{
 
     const createCheckboxFilters = (container, item, type)=>{
         container.innerHTML = ""; //Clear previous
-        items.forEach((item)=>{
+        item.forEach((item)=>{
             const label = document.createElement("label");
             label.innerHTML = `<input type="checkbox" data-filter-type="${type}" value="${item}"
             >${item}`;
@@ -99,6 +99,83 @@ document.addEventListener("DomContentLoaded",()=>{
             swatch.dataset.value = color;
             container.appendChild(swatch);
         });
-    }
+    };
+    // create color for product
+    const createColorOption = (colors)=>{
+        if(!colors || colors.length === 0) return "";
+        return colors
+        .map((color,index)=>`
+        <button 
+        class="color-option ${index === 0 ? "selected" : ""}"
+        style = "background-color:${color};"
+        data-color="${color}"
+        aria-label="Select color $(color)">
+        </button>
+        
+        `)
+        .join("");
+    };
+
+    // crate sized for product
+
+   const createSizeOptions = (sizes) => {
+    if(!sizes || sizes.length === 0) return "";
+    const sizeButtons = sizes
+    .map(
+        (size,index) =>`
+        <button class="size-option ${
+            index === 0 ? "selected" : ""
+   }">
+   ${size}
+   </button>
+   `
+    )
+    .join("");
+
+    return `
+    <div class="size-options-container">
+    <div class = "size-selecter">
+    ${sizeButtons}
+    </div>
+    </div>
+    `;
+   };
+
+   const renderProducts = (productsToRender) => {
+    productGrid.innerHTML = "";
+    productsToRender.forEach((product) => {
+        const productCart = document.createElement("div");
+        productCart.className = "product-card";
+        productCart.innerHTML = `
+        <div class="product-image-container">
+        <img 
+        class = "product-image"
+        src="${product.images[0]}"
+        alt="${product.name}"
+        >
+        <button class = "favorite-btn" arial-label="Add to favorites">
+        <i class="fa-regular fa-heart"></i>
+        </button>
+        </div>
+        <div class="product-details">
+        <div class="product-info">
+        <div class="meta-info">
+        <p class="brand">${product.brand}</p>
+        <div class="rating">
+        <i class="fa-solid fa-start"></i>
+        <span class="rating-score">${product.avgRating}
+        </span>
+        <span class="rating-reviews">(${product.numReviews})
+        </span>
+        </div>
+        </div>
+        <h4 class="product-name">
+        <a href="product.html?slug=${
+            product.slug
+        }">${product.name}</a></h4>
+        <div class="colors">
+        <p class="option-label">Colors : </p>`
+    })
+   };
 
 });
